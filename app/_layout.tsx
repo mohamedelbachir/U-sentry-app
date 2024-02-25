2cea3f86f46dbf2099ac58bf5486fdb416538273
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2024-02-25 09:35:54 +0100

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
---
+++ supabase/initSupabase.tsx
@@ -0,0 +1,2 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
