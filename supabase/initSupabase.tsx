45934b8aa1365a620eb8466666d61caea0e6214b
bachdev <76158313+mohamedelbachir@users.noreply.github.com>
2025-03-11 02:02:03 +0100

    Update initSupabase.tsx

--- supabase/initSupabase.tsx
+++ supabase/initSupabase.tsx
@@ -3,9 +3,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Database } from "./supabase";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = "https://jpgbhncvkbrgjirszsal.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwZ2JobmN2a2JyZ2ppcnN6c2FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2MTAyNjMsImV4cCI6MjAyNDE4NjI2M30.jws2goYjkBPXaPOw0fA4KC4Ji2F7O7ryFJvT1-9reyw";
+const supabaseURL = "https://mtktimngklgdhdjznaxr.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10a3RpbW5na2xnZGhkanpuYXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyNzQ2ODMsImV4cCI6MjA1Njg1MDY4M30.QNlMAMDVzCb-DEeijrBRJveVnEk7B5g4Z4KOBeQBZ2Q";
 
 const ExpoStorage = {
   setItem: async (key: string, value: string) => {
